4887b47250db58104d71de00de8c819d5fc2a1a3
kartik <kabakshi@hotmail.ca>
2021-11-09 16:23:54 -0500

    add delete button

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs" );
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.ca"
 };
 
 //Adding Routes
