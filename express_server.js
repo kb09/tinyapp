94df6285899fc196d9f15e3e5239fc76ba3d4153
kartik <kabakshi@hotmail.ca>
2021-11-08 23:30:39 -0500

    adding routes

--- express_server.js
+++ express_server.js
@@ -7,10 +7,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+//Adding Routes
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
